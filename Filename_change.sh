f95d9afa0d6ea50de4fb4a0137ceb8b38b1cefaf
hassan ghayas <hassan_ms@outlook.com>
2023-06-05 10:23:24 +0500

    changes in filename_change.sh and coverage.sh

--- Filename_change.sh
+++ Filename_change.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # run this script with fastq directory name as argument to change the name of illumina fastq files in the given directory
-ls $1 > temp.txt
-cat temp.txt | cut -dR -f1 | uniq >list.txt
-for i in $(cat list.txt); do
+
+for i in $(ls $1 | cut -dR -f1 | uniq); do
 	A=$(echo $i |cut -d_ -f1)
 	mv $1/${i}R1_001.fastq.gz $1/${A}_1.fastq.gz
 	mv $1/${i}R2_001.fastq.gz $1/${A}_2.fastq.gz
 	# echo $A
 done
-rm *.txt
--- coverage.sh
+++ coverage.sh
@@ -1,9 +1,6 @@
-mkdir output/coverages
-ls output/bamfiles >output/coverages/list.txt
-for i in $(cat output/coverages/list.txt); do
-    samtools coverage output/bamfiles/${i} >output/coverages/${i}.tab
-    samtools coverage -H output/bamfiles/${i} >>output/coverages/coverage.txt
+mkdir output/coverage
+for i in $(cat output/list.txt); do
+    samtools coverage output/bamfiles/${i}.bam >output/coverage/${i}.tab
+    awk 'FNR > 1 { print }' output/coverage/${i}.tab >>output/coverage/coverage.txt
 done
-cat output/coverages/list.txt | cut -d. -f1 >output/coverages/temp.txt
-paste output/coverages/temp.txt output/coverages/coverage.txt >output/coverages/coverage.tab
-rm ~/EF-analysis/output/coverages/*.txt
+paste output/list.txt output/coverage/coverage.txt >output/coverage/coverage.tab
