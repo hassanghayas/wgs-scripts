4d4b3ebc1bdb9e5d48c9b0df68490999e22a298f
hassan ghayas <hassan_ms@outlook.com>
2023-06-05 12:34:23 +0500

    changes for all coverage tab file

--- coverage.sh
+++ coverage.sh
@@ -2,5 +2,8 @@ mkdir output/coverage
 for i in $(cat output/list.txt); do
     samtools coverage output/bamfiles/${i}.bam >output/coverage/${i}.tab
     awk 'FNR > 1 { print }' output/coverage/${i}.tab >>output/coverage/coverage.txt
+    awk 'FNR == 1 { print }' output/coverage/${i}.tab > output/coverage/header.txt
 done
-paste output/list.txt output/coverage/coverage.txt >output/coverage/coverage.tab
+paste output/list.txt output/coverage/coverage.txt >output/coverage/temp_coverage.txt
+sed 's|#|seqID\t|' header.txt > temp.txt
+cat temp.txt temp_coverage.txt > allcoverage.tab
