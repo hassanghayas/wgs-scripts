bcb3656fe7acae145237667e0e10a75a59216d62
hassan ghayas <hassan_ms@outlook.com>
2023-06-05 12:48:35 +0500

    changes for all coverage tab file

--- coverage.sh
+++ coverage.sh
@@ -5,5 +5,6 @@ for i in $(cat output/list.txt); do
     awk 'FNR == 1 { print }' output/coverage/${i}.tab > output/coverage/header.txt
 done
 paste output/list.txt output/coverage/coverage.txt >output/coverage/temp_coverage.txt
-sed 's|#|seqID\t|' header.txt > temp.txt
-cat temp.txt temp_coverage.txt > allcoverage.tab
+sed 's|#|seqID\t|' output/coverage/header.txt > output/coverage/temp.txt
+cat output/coverage/temp.txt output/coverage/temp_coverage.txt > output/coverage/allcoverage.tab
+rm output/coverage/*.txt
